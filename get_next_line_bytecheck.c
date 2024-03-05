7ab4ae2de03c6da0668606238a89dd2ae4dd0b4f
Daniele <iatcodaniele@gmail.com>
2024-03-05 09:45:20 +0100

    Create get_next_line_bytecheck.c

---
+++ get_next_line_bytecheck.c
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bytecheck.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diatco <diatco@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/11 14:35:39 by diatco            #+#    #+#             */
+/*   Updated: 2024/02/18 14:25:30 by diatco           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*stash;
+	ssize_t byte;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		free(stash);
+		return (NULL);
+	}
+	stash = ft_read_buffer(fd, stash, &byte);
+	if (!stash)
+		return (NULL);
+	line = ft_extract_line(stash);
+	stash = ft_remove_output_line(stash);
+	printf("Bytes read: %zd\n", byte);
+	return (line);
+}
+
+char	*ft_read_buffer(int fd, char *stash, ssize_t *byte)
+{
+	char	*temp;
+	char	*read_line;
+
+	if (!stash)
+		stash = ft_calloc(1, sizeof(char));
+	temp = stash;
+	read_line = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	*byte = 1;
+	while (!ft_strchr(stash, '\n') && *byte > 0)
+	{
+		*byte = read(fd, read_line, BUFFER_SIZE);
+		if (*byte == -1)
+		{
+			free(read_line);
+			free(temp);
+			return (NULL);
+		}
+		read_line[*byte] = '\0';
+		temp = stash;
+		stash = ft_strjoin(temp, read_line);
+		free(temp);
+	}
+	free(read_line);
+	return (stash);
+}
+
+char	*ft_extract_line(char *stash)
+{
+	char	*new_line;
+	size_t	line_index;
+	size_t	stash_index;
+
+	line_index = 0;
+	stash_index = 0;
+	if (!stash[stash_index])
+		return (NULL);
+	else
+		while (stash[stash_index] && stash[stash_index] != '\n')
+			stash_index++;
+	new_line = ft_calloc(stash_index + 2, sizeof(char));
+	stash_index = 0;
+	while (stash[stash_index] && stash[stash_index] != '\n')
+	{
+		new_line[line_index++] = stash[stash_index++];
+	}
+	if (stash[stash_index] == '\n')
+		new_line[line_index] = '\n';
+	return (new_line);
+}
+
+char	*ft_remove_output_line(char *stash)
+{
+	char	*leftover;
+	size_t	stash_index;
+	size_t	leftover_index;
+
+	stash_index = 0;
+	leftover_index = 0;
+	while (stash[stash_index] && stash[stash_index] != '\n')
+		stash_index++;
+	if (stash[stash_index] == '\0')
+	{
+		free(stash);
+		return (NULL);
+	}
+	leftover = ft_calloc(ft_strlen(stash) - stash_index + 1, sizeof(char));
+	stash_index++;
+	while (stash[stash_index] != '\0')
+		leftover[leftover_index++] = stash[stash_index++];
+	free(stash);
+	return (leftover);
+}
+
+// int main()
+// {
+// 	char *line;
+// 	int fd = open("bonus1.txt", O_RDONLY);
+// 	int i = 1;
+// 	while((line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("line %i: %s\n",i, line);
+// 		free(line);
+// 		i++;
+// 	}
+
+// 	close(fd);
+// 	return (0);
+// }
